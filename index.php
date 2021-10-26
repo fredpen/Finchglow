d0f4a00664b97a87c778b0bc96b0f078d5eaea36
Oladipupo Fredrick <fredrickksola@gmail.com>
2021-10-26 11:36:56 +0100

    find index of an integer in an array

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/** Given a sorted array of distinct integers and a target value
+return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order
+ */
+
+function findIndex(array $arr, int $target)
+{
+    if (!in_array($target, $arr)) {
+        $arr[] = $target;
+        sort($arr);
+    }
+
+    return array_search($target, $arr);
+}
+
+
+// test samples
+echo json_encode(findIndex([1, 2, 6, 3, 4, 5], 1));
+echo json_encode(findIndex([1, 2, 6, 3, 4, 5], 7));
